a60c7ee590192523ab8ef1b7db89eeed0b94d262
keshavvyadavv <keshavyadavv21@gmail.com>
2026-04-09 12:48:33 +0530

    [Keshav Yadav] : Added version control setup and added eureka server

---
+++ versionservice/src/main/java/com/versionservice/VersionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.versionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionserviceApplication.class, args);
+	}
+
+}
